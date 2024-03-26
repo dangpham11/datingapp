3532040d67589635282ce37a4591d68a5e869e20
dangpham11 <156592583+dangpham11@users.noreply.github.com>
2024-03-26 11:22:01 +0700

    chapter-8

--- API/obj/Debug/net5.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net5.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+677f183eafe8e1f4889b4b2920d675e0358e3ab2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6dee63a3269b4a9e91cfa99f43662a880840147")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
